786a1ea370c5d202b2bc2c8fe111e5c30062c423
Source Academy User <Alabhya Jindal>
2022-11-05 13:56:39 +0530

    Changes made from Source Academy

--- exercises1.js
+++ exercises1.js
@@ -26,24 +26,58 @@ Exercise 1.2
 
 */
 
-/*
-Exercise 1.3
+
+// Exercise 1.3
+
+// function square (a) {
+//     return a * a;
+// }
+
+// function sum_of_squares (a, b, c) {
+//     if (a + b > a + c && a + b > b + c) {
+//         return square(a) + square(b);
+//     } else if (b + c > a + b && b + c > a + c ) {
+//         return square(b) * square(c);
+//     } else {
+//         return square(a) * square(c);
+//     }
+// }
+
+// sum_of_squares(4, 7, 2);
+
+// Exercise 1.5
+
+function p() { return p(); }
+
+function test(x, y) {
+    return x === 0 ? 0 : y;
+}
+
+// test(0, p())
+
+/* 
+Applicative order evalutation
+
+test(0, p())
+test(0, p())
+Keeps executing the same line as the p function is recurisve with no base condition
+
 */
 
-function square (a) {
-    return a * a;
-}
+/*
+Normal order evaluation
 
-function sum_of_squares (a, b, c) {
-    if (a + b > a + c && a + b > b + c) {
-        return square(a) + square(b);
-    } else if (b + c > a + b && b + c > a + c ) {
-        return square(b) * square(c);
-    } else {
-        return square(a) * square(c);
-    }
-}
+test(0, p())
+0 === 0 ? 0 : p()
+0
 
-sum_of_squares(4, 7, 2);
+*/
+
+
+
+
+
+
+
 
 
