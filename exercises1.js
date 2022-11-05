697fcc9d83d1cbf019882c7a60d562c3d18ad7ae
Alabhya Jindal <No public email provided>
2022-11-05 13:18:37 +0530

    exercises

---
+++ exercises1.js
@@ -0,0 +1,48 @@
+/*
+Exercise 1.1
+
+10
+12
+8
+3
+6
+3
+4
+19
+false
+4
+16
+6
+16
+
+*/
+
+/*
+Exercise 1.2
+
+(5 + 4 + (2 - (3 - (6 + 4 / 5))))
+/ 
+(3 * (6 - 2) * (2 - 7));
+
+*/
+
+/*
+Exercise 1.3
+*/
+
+function find_two_largest_numbers (a, b, c) {
+    // const two_largest = [];
+    // if (a > b && a > c) two_largest.push(a);
+    // if (b > a && b > c) two_largest.push(b);
+    // if (c > a && c > b) two_largest.push(c);
+    return 5, 6;
+}
+
+function sum_of_squares (a, b, c) {
+    const x, y = find_two_largest_numbers(a, b, c);
+    console.log(x, y);
+}
+
+sum_of_squares(4, 5, 6);
+
+
